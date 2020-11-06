ba4e4ad4b2ebc3d6fbb0e954ab7ce3163afd379f
=Edu <=educharrone@hotmail.com>
2020-11-06 17:01:48 -0300

    Primeira Classe Position

--- src/Projeto.java
+++ src/Projeto.java
@@ -1,9 +1,16 @@
+import boardgame.Position;
 
 public class Projeto {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	System.out.println("Teste de funcionamento ");		
+
+	Position position = new Position(3,5);
+	
+	System.out.println(position);		
+	
+	
 	}
 
 }
